ef72fb75d38e9e37afa7bd57e4426a2a1def18be
developer <hjgguge@gmail.com>
2018-11-10 13:29:45 +0800

    fux

--- rust_demo/src/main.rs
+++ rust_demo/src/main.rs
@@ -1,4 +1,27 @@
 fn main() {
-    println!("Hello, world!");
-    println!("this is muy first into rust");
+
+    //初体验
+//    println!("Hello, world!");
+//    println!("this is muy first into rust");
+
+
+    //变量
+//    let mut x = 5;
+//    println!("The value of x is: {}", x);
+//    x = 6;
+//    println!("The value of x is: {}", x);
+
+//    loop {
+//        println!("again!");
+//    }
+
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{}!", number);
+
+        number = number - 1;
+    }
+
+    println!("LIFTOFF!!!");
 }
